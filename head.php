e1a38ba2deb04f43e4ee3b23c41b413f5615d0ca
aeirou <aeironefelipee@gmail.com>
2024-07-23 22:58:53 +1200

    js script added

---
+++ head.php
@@ -0,0 +1,15 @@
+<body>
+    <nav class="navbar-expand-lg bg-body-secondary p-3 sticky-top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-3">
+                    <a class="navbar-brand-two me-auto d-sm-flex" href="#">                
+                        <img src="static/navbar/100logogold-nav.svg" alt="">
+                        <div class="vr mb-1 bg-dark"></div>
+                        <img src="static/navbar/motto-nav.svg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</body>
--- index.php
+++ index.php
@@ -1,79 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+include'includes/basehead.html'
+?>
+
 <head>
-    <title>Homepage</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="main.css">
+    <title>Christchurch Adventist School</title>
 </head>
 
-<!-- <div class="bg-img img-fluid w-100 vh-100 d-flex justify-content-center align-items-center"></div> -->
-
 <body>
-    <nav class="navbar-expand-lg bg-body-secondary p-4 sticky-top">
-        <div class="navbar container-fluid">
-            <a class="navbar-brand" href="">
-                <div class="logo-na d-inline-block align-top">
-                    <img src="static/navbar/100logogold-nav.svg" alt="">
-                </div>
-                <div class="vr mb-1 d-inline-block align-top"></div>
-                <div class="motto-nav d-inline-block align-top">
-                    <img src="static/navbar/motto-nav.svg" alt="">
-                </div>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
-                </li>
-                <li class="nav-item">
-                <!-- <a class="nav-link" href="#">Link</a> -->
-                </li>
-                <li class="nav-item dropdown">
-                <!-- <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> -->
+
+    <?php
+    require_once'head.php'
+    ?>
+
+    <div class="align-item-start p-5">
+            <div class="hstack gap-3">
+                <p class="lead text-secondary mb-4 text-shadow-1">Christchurch Adventist School</p>
+                <div class="vr mb-4"></div>
+                <p class="lead mb-4 text-secondary text-shadow-1 fw-bold">100th Anniversary</p>
             </div>
         </div>
-    </nav>
+
     <div class="welcome-banner">
         <div class="row p-5">
-            <div class="logo col d-flex justify-content-center">
+            <div class="frontlogo col-6 d-flex justify-content-center">
                 <img src="static/100logomotto.svg" alt="">
-            </div>
-            <div class="motto col d-flex justify-content-center">
-                <img src="static/mottonobg.svg" alt="">
             </div>
         </div>
     </div>
-    <div class="row align-item-start p-5">
-            <div class="hstack gap-3">
-                <p class="lead text-secondary mb-4 text-shadow-1">CAS</p>
-                <div class="vr mb-4"></div>
-                <p class="lead mb-4 text-secondary text-shadow-1 fw-bold">Educating for Eternity</p>
-            </div>
-        </div>
+
+    <h1 id="test-header">Hello, World!</h1>
+
+    <script>
+        // Test jQuery functionality
+        $(document).ready(function(){
+            // Change the text of the header
+            $("#test-header").text("jQuery is working!");
+            
+            // Display an alert to confirm jQuery is loaded
+            alert("jQuery ifasdfass successfully loaded and working!");
+        });
+    </script>
 
 </body>
+
 </html>
